aac97f0d0ef61eb85409edb428fbc79a17a7ee08
aiko marques <mutantetrakor@gmail.com>
2024-06-01 13:04:51 -0300

    aula_1

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,11 @@
+numero_1 = 10
+numero_2 = 20
+
+print (numero_1 + numero_2)
+print (numero_1 - numero_2)
+print (numero_1 * numero_2)
+print (numero_1 / numero_2)
+print (numero_1 // numero_2)
+print (numero_1 % numero_2)
+print (numero_1 ** numero_2)
+
